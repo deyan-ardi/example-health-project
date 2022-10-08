4c307a192ba7c8fe551dc3619359006fd2a3a307
deyan-ardi <riyan.clsg11@gmail.com>
2022-10-08 12:17:28 +0800

    fix navbar

--- user/component/_navbar.php
+++ user/component/_navbar.php
@@ -1,15 +1,24 @@
  <header>
-     <div class="brand">
-         <div class="row">
-             <a href="#"><img src="../assets/img/logo-text-less.jpeg" height="60px" alt="logo"></a>
-             <h4>RUSSEL STREET MEDICAL CENTRE</h4>
+     <nav class="navbar">
+         <!-- LOGO -->
+         <div class="container">
+             <div class="navbar-content">
+                 <div class="logo">
+                     <img src="../assets/img/logo-text-less.png" class="log-image" width="80px" alt="logo">
+                     <p class="logo-text">RUSSEL STREET <br> MEDICAL CENTRE</p>
+                 </div>
+                 <!-- NAVIGATION MENU -->
+                 <ul class="nav-links">
+                     <!-- USING CHECKBOX HACK -->
+                     <input type="checkbox" id="checkbox_toggle" />
+                     <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+                     <!-- NAVIGATION MENUS -->
+                     <div class="menu">
+                         <li><a href="index">HOME</a></li>
+                         <li><a href="booking">BOOKING</a></li>
+                     </div>
+                 </ul>
+             </div>
          </div>
-     </div>
-
-     <nav>
-         <ul>
-             <li><a href="index">HOME</a></li>
-             <li><a href="booking">BOOKING</a></li>
-         </ul>
      </nav>
  </header>
